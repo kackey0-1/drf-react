1cf37e3e15b26ae4dc5607f728fba35f2914a22c
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-02-06 22:15:53 +0900

    adding action and store.

--- src/jsx/pages/Todos.jsx
+++ src/jsx/pages/Todos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Todo from '../components/Todo'
+import * as TodoActions from '../actions/TodoActions'
 import TodoStore from '../stores/TodoStore'
 
 export default class Todos extends React.Component {
@@ -21,14 +22,24 @@ export default class Todos extends React.Component {
     })
   }
 
+  createTodo() {
+    TodoActions.createTodo('New Todo')
+  }
+  
+  reloadTodos() {
+    TodoActions.reloadTodos()
+  }
+
   render() {
-    const { todos } = this.state
+    const { todos } = this.state;
     const TodoComponents = todos.map((todo) => {
-      return <Todo key={todo.id} {...todo} />
-    })
+      return <Todo key={todo.id} {...todo}/>;
+    });
 
     return (
       <div>
+        <button onClick={this.createTodo.bind(this)}>Create!</button>
+        <button onClick={this.reloadTodos.bind(this)}>Reload!</button>
         <h1>Todos</h1>
         <ul>{TodoComponents}</ul>
       </div>
